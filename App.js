50eb5677a93fee2627e3435dca72ba35c0cf247c
Himanshi Sharma <himanshi.sharma@hashedin.com>
2019-08-29 12:14:58 +0530

    basic form created

--- App.js
+++ App.js
@@ -1,32 +1,22 @@
 import React, { Component } from 'react';
-import { Button , StyleSheet, Text, View } from 'react-native';
+import { 
+  Button ,
+  StyleSheet,
+   Text,
+   View,
+   TouchableOpacity   
+  } from 'react-native';
+import  RegForm from './component/regForm';
 
 export default class ButtonDemo extends Component {
 
-  _onPressButton() {
-    alert('You tapped a button!');
-  }
-
   render() {
     return (
-      <View style={StyleSheet.container}>
-        <View style={StyleSheet.buttonContainer}>
-          <Button 
-            onPress = {this._onPressButton}
-            title="Click me"
-          />
-        </View>
+      <View>
+        <RegForm />
       </View>
+      
     );
   }
 } 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  buttonContainer: {
-    margin: 20
-  }
-});
